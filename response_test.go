dc6d4bff013e5aa7ce99cc27e7901fde53ddc8aa
hslam <791874158@qq.com>
2021-06-22 22:23:03 +0800

    add write empty data test

--- response_test.go
+++ response_test.go
@@ -138,6 +138,7 @@ func TestResponse(t *testing.T) {
 	m.HandleFunc("/msg", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set(contentLength, strconv.FormatInt(int64(len(msg)), 10))
 		w.Write(msg)
+		w.Write(nil)
 	})
 	m.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set(contentLength, "invalid content length header")
